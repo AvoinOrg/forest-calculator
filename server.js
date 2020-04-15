57c0333479def02dedb77f22548f52988c319ae0
xbexbex <petja.valkama@gmail.com>
2020-04-15 15:59:59 +0300

    Fix cors

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app
 
     server.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
 
