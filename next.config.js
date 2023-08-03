ccf8583cf3c8e6a936aefcab63e00cb55dbc6061
Petja <petja@avoin.org>
2023-08-03 23:14:20 +0300

    Update API URL

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ module.exports = withImages(
       return config;
     },
     env: {
-      API_URL:
-        process.env.NODE_ENV === "production"
-          ? process.env.API_URL + "/api"
-          : "http://localhost:3000/api"
+      NEXT_PUBLIC_API_URL:
+        process.env.NODE_ENV === "dev"
+        ? "http://localhost:3000/api"
+        : process.env.NEXT_PUBLIC_API_URL + "/api"
     }
   })
 );
